2d4cc75e713e83efb21c1704699cadebd1404eb6
Katarzyna Da Silva <conic916@gmail.com>
2019-07-01 19:53:47 +0200

    Add

---
+++ client/modules/Lane/Lane.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+// Import Style
+import styles from './Lane.css';
+
+class Lane extends Component {
+  render() {
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+Lane.propTypes = {
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Lane);
